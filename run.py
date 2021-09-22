65b326554a7e93ddda72a047abb2f5ec973f1849
dhakal79 <n.dhakal@un-ihe.org>
2021-09-22 21:29:40 +0000

    connect API and confrm access to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import json
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+creds = json.load(open('creds.json'))
+CREDS = Credentials.from_service_account_info(creds)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("coagulant_dose")
+
+pHRAW = SHEET.worksheet("pHRAW")
+
+data = pHRAW.get_all_values()
+print(data)
