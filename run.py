c71542e75d5feb72cad5e6375da9b228c6ef173b
dhakal79 <n.dhakal@un-ihe.org>
2021-09-25 21:26:12 +0000

    add get experiemtnal data function to get the data from user

--- run.py
+++ run.py
@@ -13,8 +13,16 @@ CREDS = Credentials.from_service_account_info(creds)
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("coagulant_dose")
-
+"""
 pHRAW = SHEET.worksheet("pHRAW2")
 
 data = pHRAW.get_all_values()
 print(data)
+"""
+def get_exp_data():
+    print("Please enter lab experimental data")
+    print("data shour be three numbers separated by commas")
+    print("Example:0,7.5,105\n")
+    data_str = input("Enter your data here:")
+    print(f"The data provided is {data_str}")
+get_exp_data()
