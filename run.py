cc3311faaa6c9a0865b7fc05dad9affa0d04a1f7
dhakal79 <n.dhakal@un-ihe.org>
2021-09-25 22:21:00 +0000

    add loop that runs until the data provided by users is valid

--- run.py
+++ run.py
@@ -20,22 +20,29 @@ data = pHRAW.get_all_values()
 print(data)
 """
 def get_exp_data():
-    print("Please enter lab experimental data")
-    print("data should be three numbers separated by commas")
-    print("Example:0,7.5,105\n")
-    data_str = input("Enter your data here:")
+    while True:
+        print("Please enter lab experimental data")
+        print("data should be three numbers separated by commas")
+        print("Example:0,7.5,105\n")
+        data_str = input("Enter your data here:")
 
-    exp_data = data_str.split(",")
-    validate_data(exp_data)
+        exp_data = data_str.split(",")
+        if validate_data(exp_data):
+            print("data is valid")
+            break
+
+    return exp_data
 
 def validate_data(values):
 
     try:
+        [int(value) for value in values]
         if len(values) != 3:
             raise ValueError(
                 f"Exactly 3 values are required, you provided {len(values)}")
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
-        
+        return False
+    return True    
 get_exp_data()
