facaabbe87220cab95f5f5f739b541d7853a24ac
dhakal79 <n.dhakal@un-ihe.org>
2021-09-29 22:57:47 +0000

    add a function to get data for dose calcuation, data validation

--- run.py
+++ run.py
@@ -55,10 +55,59 @@ def plot_sheet(sheet_name):
         if optimum_row is not None:
             print(f"Optimum dose {cog[optimum_row]} for {sheet_name}")
     except ValueError:
-        print("Data is not valid !")
+        print("Data is not valid ! Please check the data entry")
 
 
 plot_sheet('pHRAW')
 plot_sheet('pHRAW2')
 plot_sheet('pHRAW3')
 plot_sheet('pHRAW4')
+
+
+# 'calculating the amount of coagulant dose for a give water flow'
+def get_dose_data(sheet):
+    dose = SHEET.worksheet(sheet)
+    data_cal = dose.get_all_values()
+    return data_cal
+
+
+def dose_calcualtion_data():
+    """
+    Get dose calcualtion input from the user.
+    We run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+
+    while True:
+        print("Please enter data from the given condition.")
+        print("Data should be three numbers, separated by commas.")
+        print("Example: 1,2,3\n")
+        data_str = input("Enter your data here: \n")
+
+        dose_data = list(data_str.split(","))
+
+        if validate_data(dose_data):
+            break
+
+    return dose_data
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values are required, you provided {len(values)}")
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+        return False
+
+    return True
+
